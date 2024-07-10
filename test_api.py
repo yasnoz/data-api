1e8220f7bfcddc6f519e0a2b07b4777d29cdb0bb
Yasin Oz <yasnoz@gmail.com>
2024-07-10 16:45:56 +0200

    Progress on API: XXX tests passing

--- test_api.py
+++ test_api.py
@@ -1,4 +1,9 @@
 # pylint: disable=missing-docstring,invalid-name
 
-# TODO: paste the code from Kitt's instructions
 
+import requests
+
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+city = response[0]
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
--- weather.py
+++ weather.py
@@ -1,7 +1,6 @@
 # pylint: disable=missing-module-docstring
 
 import sys
-import urllib.parse
 import requests
 
 BASE_URI = "https://weather.lewagon.com"
@@ -12,19 +11,33 @@ def search_city(query):
     Look for a given city. If multiple options are returned, have the user choose between them.
     Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    city_url = f"https://weather.lewagon.com/geo/1.0/direct?q={query}&limit=5"
+    response = requests.get(city_url).json()
+    if len(response) == 1:
+        return response[0]
+    if len(response) > 1:
+        for i, city in enumerate(response):
+            print({i+1}, city["name"])
+        choice = int(input("Choose your city"))-1
+        return response[choice]
+    return None
+
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+
+    url = f"https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}"
+    response = requests.get(url).json()
+    return response["list"][::8]
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
+    forecast = weather_forecast(city['lat'], city['lon'])
 
-    # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    for weather in forecast:
+        print(weather["dt_txt"][:9]+": " + weather["weather"][0]["description"]+ " " + str(weather["main"]["temp"]))
 
 if __name__ == '__main__':
     try:
